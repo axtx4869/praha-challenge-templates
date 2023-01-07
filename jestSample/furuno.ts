6e074e31df1f992ee5f34ed64b2d4aae19d73512
william <shota.furuno@claves.co.jp>
2023-01-07 18:26:58 +0900

    素数の配列を返す関数を定義した

--- jestSample/furuno.ts
+++ jestSample/furuno.ts
@@ -1,14 +1,28 @@
-import { ZipcodeApiServiceImpl, ZipcodeApiService } from "./zipCodeApiService";
+import { ZipcodeApiServiceImpl, ZipcodeApiService } from "./zipcodeApiService";
 
 export const haveHumanRight = (height: number, gender: "MALE" | "FEMALE" | "OTHER"): boolean =>
   !(gender === "MALE" && height < 170);
 
-export const comma = (num?: number | string): string => {
-  if (!num) {
-    throw new Error("num is required");
+export const getPrimeNumbers = (numbers: number[]): number[] => {
+  if (numbers.length === 0) {
+    throw new Error("numbers should not be empty");
   }
-  const numericValue = typeof num === "number" ? num : Number(num);
-  return numericValue.toLocaleString();
+
+  const primeNumbers = [];
+  for (const number of numbers) {
+    if (number === 1) {
+      primeNumbers.push(number);
+    }
+    for (let i = 2; i < number; i++) {
+      if (number % i === 0) {
+        break;
+      }
+      if (i === number - 1) {
+        primeNumbers.push(number);
+      }
+    }
+  }
+  return primeNumbers;
 };
 
 export const getPrefectureNameByZipcode = async (
